181841cbc3a33e79bb1f3430a7f8d7731eb9a65c
Claire <claire.lindblom@gmail.com>
2017-09-27 14:39:24 -0700

    Calls data and puts it into a table with each being a different type

--- index.php
+++ index.php
@@ -8,49 +8,42 @@
 <?php
 
 $dailies = json_decode(file_get_contents("https://api.guildwars2.com/v2/achievements/daily/tomorrow"));
-// print_r($dailies);
+
 $dailyIDs = '';
 foreach ($dailies as $gameMode) {
 	foreach ($gameMode as $achievement) {
 		$dailyIDs .= $achievement->id .',';
 	}
 }
-print_r($dailyIDs);
-/*
-
-*/
-foreach ($results->wvw as $value) {
-	//echo $value->id;
-
-	$achievement = json_decode(file_get_contents("https://api.guildwars2.com/v2/achievements?id=".$value->id));
-
-	// print_r($achievement);
-	/*
-	Things to display
-	Name, Requirement, Description
-	*/
-
-	/*
-
-	use $results (of next daily) to get all ids in a comma separated string
-	query api using above, insert into an associative array of achieve_id=>data
-	$allAchieves = apiCall with all ids
-		ids 		achievement
-		1999		blah blah Caravan Disruptor
-		1849 		blah blah land claimer
-		2304 		blah blah pve scrubiness
-		34324		blah blah spvp newbnessiness
-
-	foreach results as gameMode
-		foreach gameMode as data
-			$allAchieves[d$ata->id] will contain your data
-			e.g. $allAchieves[1999] is your WvW daily
-		*/
-	?>
-	</br>
-<?php
+// get all of the achievement data from the API - returns array of objects, indexed numerically
+$allAchieves = json_decode(file_get_contents("https://api.guildwars2.com/v2/achievements?ids=".$dailyIDs));
+// start a new array to hold the properly indexed achievements - indexing by their ids
+$achievesArray = array();
+// loop through the array of objects and add them to the id-indexed array
+foreach ($allAchieves as $achieve) {
+	$achievesArray[$achieve->id] = $achieve;
 }
+unset($allAchieves); // just so we know, we aren't using this anymore!
+//
 ?>
+<table>
+<?php foreach ($dailies as $gamemode) { ?>
+	<tr>
+	<?php foreach($gamemode as $daily) { ?>
+		<tr>
+			<td> <?= $achievesArray[$daily->id]->name ?> </td>
+			<td> <?= $achievesArray[$daily->id]->requirement ?> </td>
+			<td> <?= $achievesArray[$daily->id]->description ?> </td>
+		</tr>
+	<?php } ?>
+	</tr>
+<?php } ?>
+</table>
 
 </body>
 </html>
+
+	<!-- /*
+	Things to display
+	Name, Requirement, Description
+	*/ -->
