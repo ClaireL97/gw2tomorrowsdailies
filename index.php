7c491309b35e30f80a09d40971eeb80025be4f7f
Claire <claire.lindblom@gmail.com>
2017-10-03 10:05:45 -0700

    adds separate tables for each gametype

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<title></title>
 </head>
 <body>
-<h1>Tomorrows Dailies</h1>
+<h1 style="color:red">Tomorrows Dailies</h1>
 <?php
 
 $dailies = json_decode(file_get_contents("https://api.guildwars2.com/v2/achievements/daily/tomorrow"));
@@ -28,25 +28,86 @@ foreach ($allAchieves as $achieve) {
 unset($allAchieves); // just so we know, we aren't using this anymore!
 //
 ?>
-<table>
-<thead>
-<th> Name </th>
-<th> Requirement </th>
-<th> Description </th>
-</thead>
-<?php foreach ($dailies as $gamemode) { ?>
-	<tr>
-	<?php foreach($gamemode as $daily) { ?>
+<div style="border:1px solid black; width:600px;height:600px;overflow:scroll;">
+	<table class="table table-bordered table-striped">
+	<thead>
+	<th> Name </th>
+	<th> Requirement </th>
+	<th> Description </th>
+	<!-- <th> Required Access</th> -->
+	</thead>
+		<?php foreach($dailies->pve as $daily) { ?>
 		<tr>
-			<td> <?= $achievesArray[$daily->id]->name ?> </td>
-			<td> <?= $achievesArray[$daily->id]->requirement ?> </td>
-			<td> <?= $achievesArray[$daily->id]->description ?> </td>
+			<tr>
+				<td> <?= $achievesArray[$daily->id]->name ?> </td>
+				<td> <?= $achievesArray[$daily->id]->requirement ?> </td>
+				<td> <?= $achievesArray[$daily->id]->description ?> </td>
+				<!-- <td> <?php print_r($daily->required_access); ?> </td>  -->
+			</tr>
 		</tr>
 	<?php } ?>
-	</tr>
-<?php } ?>
-</table>
-
+	</table>
+</div>
+<div style="border:1px solid black; width:600px;height:600px;overflow:scroll;">
+	<table class="table table-bordered table-striped">
+	<thead>
+	<th> Name </th>
+	<th> Requirement </th>
+	<th> Description </th>
+	<!-- <th> Required Access</th> -->
+	</thead>
+		<?php foreach($dailies->wvw as $daily) { ?>
+		<tr>
+			<tr>
+				<td> <?= $achievesArray[$daily->id]->name ?> </td>
+				<td> <?= $achievesArray[$daily->id]->requirement ?> </td>
+				<td> <?= $achievesArray[$daily->id]->description ?> </td>
+				<!-- <td> <?php print_r($daily->required_access); ?> </td>  -->
+			</tr>
+		</tr>
+	<?php } ?>
+	</table>
+</div>
+<div style="border:1px solid black; width:600px;height:600px;overflow:scroll;">
+	<table class="table table-bordered table-striped">
+	<thead>
+	<th> Name </th>
+	<th> Requirement </th>
+	<th> Description </th>
+	<!-- <th> Required Access</th> -->
+	</thead>
+		<?php foreach($dailies->pvp as $daily) { ?>
+		<tr>
+			<tr>
+				<td> <?= $achievesArray[$daily->id]->name ?> </td>
+				<td> <?= $achievesArray[$daily->id]->requirement ?> </td>
+				<td> <?= $achievesArray[$daily->id]->description ?> </td>
+				<!-- <td> <?php print_r($daily->required_access); ?> </td>  -->
+			</tr>
+		</tr>
+	<?php } ?>
+	</table>
+</div>
+<div style="border:1px solid black; width:600px;height:600px;overflow:scroll;">
+	<table class="table table-bordered table-striped">
+	<thead>
+	<th> Name </th>
+	<th> Requirement </th>
+	<th> Description </th>
+	<!-- <th> Required Access</th> -->
+	</thead>
+		<?php foreach($dailies->fractals as $daily) { ?>
+		<tr>
+			<tr>
+				<td> <?= $achievesArray[$daily->id]->name ?> </td>
+				<td> <?= $achievesArray[$daily->id]->requirement ?> </td>
+				<td> <?= $achievesArray[$daily->id]->description ?> </td>
+				<!-- <td> <?php print_r($daily->required_access); ?> </td>  -->
+			</tr>
+		</tr>
+	<?php } ?>
+	</table>
+</div>
 </body>
 </html>
 <?php include "./static/includes/footer.php";?>
